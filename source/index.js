ad2487ec0b8e343f97c69cd3cbcada7823afe428
Perry Mitchell <perry@perrymitchell.net>
2018-09-10 22:34:55 +0300

    Add dir structure

---
+++ source/index.js
@@ -0,0 +1,16 @@
+/**
+ * Create an Express Router instance for use with an express app
+ * @param {Vulpes.Service} vulpesService The Vulpes service to create the app for
+ * @returns {Express.Router} An express router
+ * @example
+ *  const router = createVulpesRouter(service);
+ *  app.use("/", router);
+ * @see https://expressjs.com/en/guide/routing.html#express-router
+ */
+function createVulpesRouter(vulpesService) {
+
+}
+
+module.exports = {
+    createVulpesRouter
+};
