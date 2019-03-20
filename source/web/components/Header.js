16197734c26ecdc2f42305f0c76bd77727cbf589
Perry Mitchell <perry@perrymitchell.net>
2019-03-20 08:05:54 +0200

    Use props for handling menu clicks

--- source/web/components/Header.js
+++ source/web/components/Header.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Alignment, Button, Navbar } from "@blueprintjs/core";
 
@@ -12,25 +13,67 @@ const Container = styled.div`
     margin-bottom: 20px;
 `;
 
-export default () => (
-    <Container>
-        <Navbar>
-            <Navbar.Group align={Alignment.LEFT}>
-                <Navbar.Heading>
-                    <h3>Vulpes</h3>
-                </Navbar.Heading>
-                <Navbar.Divider />
-                <a href="/#">
-                    <Button className="bp3-minimal" icon="home" text="Dashboard" />
-                </a>
-                <a href="/#jobs">
-                    <Button className="bp3-minimal" icon="numbered-list" text="Jobs" />
-                </a>
-                <Button disabled={true} className="bp3-minimal" icon="align-left" text="Log" />
-                <Button disabled={true} className="bp3-minimal" icon="console" text="Terminal" />
-                <Button disabled={true} className="bp3-minimal" icon="cell-tower" text="Hooks" />
-                <Button disabled={true} className="bp3-minimal" icon="settings" text="System" />
-            </Navbar.Group>
-        </Navbar>
-    </Container>
-);
+export default class Header extends PureComponent {
+    static propTypes = {
+        onClickHome: PropTypes.func.isRequired,
+        onClickJobs: PropTypes.func.isRequired,
+        onClickScheduling: PropTypes.func.isRequired
+    };
+
+    render() {
+        return (
+            <Container>
+                <Navbar>
+                    <Navbar.Group align={Alignment.LEFT}>
+                        <Navbar.Heading>
+                            <h3>Vulpes</h3>
+                        </Navbar.Heading>
+                        <Navbar.Divider />
+                        <Button
+                            className="bp3-minimal"
+                            icon="home"
+                            text="Dashboard"
+                            onClick={this.props.onClickHome}
+                        />
+                        <Button
+                            className="bp3-minimal"
+                            icon="property"
+                            text="Jobs"
+                            onClick={this.props.onClickJobs}
+                        />
+                        <Button
+                            className="bp3-minimal"
+                            icon="timeline-events"
+                            text="Scheduling"
+                            onClick={this.props.onClickScheduling}
+                        />
+                        <Button
+                            disabled={true}
+                            className="bp3-minimal"
+                            icon="align-left"
+                            text="Log"
+                        />
+                        <Button
+                            disabled={true}
+                            className="bp3-minimal"
+                            icon="console"
+                            text="Terminal"
+                        />
+                        <Button
+                            disabled={true}
+                            className="bp3-minimal"
+                            icon="cell-tower"
+                            text="Hooks"
+                        />
+                        <Button
+                            disabled={true}
+                            className="bp3-minimal"
+                            icon="settings"
+                            text="System"
+                        />
+                    </Navbar.Group>
+                </Navbar>
+            </Container>
+        );
+    }
+}
--- source/web/containers/Header.js
+++ source/web/containers/Header.js
@@ -1,7 +1,16 @@
 import { connect } from "react-redux";
+import { push } from "react-router-redux";
 import Header from "../components/Header.js";
 
 export default connect(
     (state, ownProps) => ({}),
-    {}
+    {
+        onClickHome: () => dispatch => {
+            dispatch(push("/"));
+        },
+        onClickJobs: () => dispatch => {
+            dispatch(push("/jobs"));
+        },
+        onClickScheduling: () => dispatch => {}
+    }
 )(Header);
