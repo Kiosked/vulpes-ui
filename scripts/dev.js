30df146d03269e24723dbe467d1bff1ef6f1d507
Perry Mitchell <perry@perrymitchell.net>
2019-03-20 10:58:42 +0200

    Use MemoryStorage temporarily for development

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const express = require("express");
-const { Service, FileStorage } = require("vulpes");
+const { Service, FileStorage, MemoryStorage } = require("vulpes");
 const { createVulpesRouter } = require("../source/index.js");
 
 Object.assign(global, { __DEV__: true });
 
 const app = express();
-const storage = new FileStorage(path.resolve(__dirname,"../test/data/jobs.db.json"));
+// const storage = new FileStorage(path.resolve(__dirname, "../test/data/jobs.db.json"));
+const storage = new MemoryStorage();
 const service = new Service(storage);
 
 service
